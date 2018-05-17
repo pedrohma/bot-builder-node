e361fb511735e82cb267560863f27a0d77d73ca1
Pedro Henrique de Miranda Arthur <pedro@aceelectronix.com>
2018-05-17 11:19:32 -0700

    tracking request not working

--- chat.js
+++ chat.js
@@ -1,5 +1,7 @@
+var uspsTracking = require('./usps.js');
+
 module.exports = function (server, builder, connector) {
-    
+
     // Listen for messages from users 
     server.post('/api/messages', connector.listen());
 
@@ -13,18 +15,63 @@ module.exports = function (server, builder, connector) {
     ]).set('storage', inMemoryStorage);
 
     bot.dialog('greetings', [
+
+        function (session) {
+            session.sendTyping();
+            setTimeout(function () {
+                builder.Prompts.text(session, 'Hello! What is your name?');
+            }, 1500);
+
+        },
+
+        function (session, results) {
+            session.sendTyping();
+            setTimeout(function () {
+                session.send("Hello %s!", results.response);
+                session.beginDialog('choices', session.userData.profile);
+            }, 1500);
+        }
+
+    ]).reloadAction('startOver', 'Ok, starting over.', {
+        matches: /^start over$/i,
+        dialogArgs: {
+            isReloaded: true
+        }
+    });
+
+    bot.dialog('choices', [
+        function (session) {
+            builder.Prompts.choice(session, "How can I help you today?", ["Track a package", "Calculate shipping cost"], { listStyle: builder.ListStyle.button });
+        }
+        , function (session, results) {
+            switch (results.response.entity) {
+                case "Track a package":
+                    session.beginDialog('track_a_package', session.userData.profile);
+                    break;
+                case "Calculate shipping cost":
+                    session.send("I'm still learning how to calculate shipping cost... Try again.");
+                    session.replaceDialog('choices', { reprompt: true });
+                    break;
+            }
+        }
+    ]);
+
+    bot.dialog('track_a_package', [
         // Step 1
         function (session) {
-            builder.Prompts.text(session, 'Hi! What is your name?');
+            builder.Prompts.text(session, 'Can you please provide me your tracking number?');
         },
         // Step 2
         function (session, results) {
-            session.send("Hello %s", results.response);
-            builder.Prompts.choice(session, "Which is your favorite color?", ["red", "green", "blue"], { listStyle: builder.ListStyle.button });
-        }
-        // Step 3
-        , function (session, results) {
-            session.endDialog(`Your favorite color is ${results.response.entity}!`);
+            session.send("Thank you... Please wait while I'm processing your request...");
+            session.sendTyping();
+            var tracking = results.response;
+            var data = uspsTracking(tracking);
+
+            console.log(data);
+            session.send(data);
+            session.endDialog();
+            console.log('finished');
         }
     ]);
 }
--- usps.js
+++ usps.js
@@ -1,9 +1,29 @@
-var request = require('request');
+const https = require('https');
 
-var url = "http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=<TrackRequest USERID=\"YOUR_USER_ID\"><TrackID ID=\"YOUR_TRACKING_NUMBER\"></TrackID></TrackRequest>";
+module.exports = function (tracking) {
 
-request(url, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+  console.log('start here');
+
+  var url = "https://production.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=<TrackRequest USERID=\"YOUR_USER_ID\"><TrackID ID=\"" + tracking + "\"></TrackID></TrackRequest>";
+
+  let data = '';
+
+  https.get(url, (resp) => {
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      console.log(JSON.parse(data).explanation);
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+
+  return data;
+}
+
